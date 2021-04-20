68d21057217805ba2b5aff076591bb4085c2683a
Jacob Hardman <jacobhardmanbusiness@gmail.com>
2021-04-20 14:51:41 -0600

    create project and class files

---
+++ src/Calculator.java
@@ -0,0 +1,29 @@
+/**********************************************************************
+*                 Final Project -- GUI Calculator                     *
+*                                                                     *
+* PROGRAMMER:        Jacob Hardman - hard7293@bears.unco.edu          *
+* CLASS:             CS200 – Object Oriented Programming              *
+* INSTRUCTOR:        Dean Zeller                                      *
+* TERM:              Spring 2021                                      *
+* SUBMISSION DATE:   5/1/2021		                                  *
+*                                                                     *
+* DESCRIPTION:                                                        *
+* The following is an OOP definition for a Calculator built using the *
+* Swing library.                                                      *
+*                                                                     *
+* ATTRIBUTES:                                                         *
+*                                                                     *
+* COPYRIGHT:                                                          *
+* This program uses the open source MIT license. See LICENSE.txt for  *
+* more information.                                                   *
+*                                                                     *
+**********************************************************************/
+
+public class Calculator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/GUI.java
@@ -0,0 +1,24 @@
+/**********************************************************************
+*                 Final Project -- GUI Calculator                     *
+*                                                                     *
+* PROGRAMMER:        Jacob Hardman - hard7293@bears.unco.edu          *
+* CLASS:             CS200 – Object Oriented Programming              *
+* INSTRUCTOR:        Dean Zeller                                      *
+* TERM:              Spring 2021                                      *
+* SUBMISSION DATE:   5/1/2021		                                  *
+*                                                                     *
+* DESCRIPTION:                                                        *
+* The following is an OOP definition for a GUI built using the Swing  *
+* library for use in a Calculator program.                            *
+*                                                                     *
+* ATTRIBUTES:                                                         *
+*                                                                     *
+* COPYRIGHT:                                                          *
+* This program uses the open source MIT license. See LICENSE.txt for  *
+* more information.                                                   *
+*                                                                     *
+**********************************************************************/
+
+public class GUI {
+
+}
